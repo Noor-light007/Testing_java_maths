0dc0d53d091f8ca8fb237748d725617e2c0417e4
Harnoor <sandman42771@gmail.com>
2024-11-07 14:19:30 +0530

    this is the second change

--- untitled/src/Devine.java
+++ untitled/src/Devine.java
@@ -1,4 +1,15 @@
-
+class maths{
+    public static int square(int a){
+        return a * a;
+    }
+    public static boolean is_perfect(int a){
+        int test = (int)Math.sqrt(a);
+        if(test * test == a){
+            return true;
+        }
+        return false;
+    }
+}
 public class Devine {
     public static void main (String[] args){
         System.out.println("Welcome we will make mathematical codes ready for testing of git");
